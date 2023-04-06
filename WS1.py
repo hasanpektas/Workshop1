32dbd51a850a2bdbf6bc69a37e4dd4f236d93fc2
hasanpektas <121904493+hasanpektas@users.noreply.github.com>
2023-04-06 09:29:07 +0300

    Create WS1.py

---
+++ WS1.py
@@ -0,0 +1,8 @@
+users = []
+
+for i in range(10):
+    names = input("Enter Name:")
+    surnames = input("Enter Surname:")
+    users.append((names,surnames))
+
+print ("List of the names and surnames of user",users)
